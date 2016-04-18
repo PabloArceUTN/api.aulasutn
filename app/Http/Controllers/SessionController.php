6f91177959f14657f4ebb7c1248c8846d59eae62
Pablo Arce Cascante <josepa_arce@hotmail.com>
2016-04-18 01:16:42 -0600

    Logout

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -5,13 +5,38 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use Session;
 
 class SessionController extends Controller
 {
-    
-    public function index()
-    {
-      # code...
+
+  public function __construct(Request $request)
+  {
+    //Call for token authentication before execute the rest of the controller
+    Parent::InitAuth($request);
+  }
+
+  public function index()
+  {
+    # code...
+  }
+
+  // This destroy the user session
+  public function LogOut(Request $request)
+  {
+    try {
+      $dots = str_replace('.', '', $request->input('token'));
+      echo $dots;
+      var_dump(Session::all());
+      if (Session::has($dots)) {
+        Session::forget($dots);
+        return response()->json(['message' => 'logout'], 200);
+      }else {
+        return response()->json(['message' => 'session_not_exist'], 200);
+      }
+    } catch (Exception $e) {
+      return response()->json(['error' => '$e'], 422);
     }
+  }
 
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -55,4 +55,5 @@ Route::group(['prefix' => 'api'], function()
 {
     Route::resource('authenticate', 'AuthenticateController', ['only' => ['index']]);
     Route::post('authenticate', 'AuthenticateController@authenticate');
+    Route::post('logout', 'SessionController@LogOut');
 });
